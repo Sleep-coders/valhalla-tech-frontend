d3750b2f7d8e77e197b24d6f62d3cd0a458255ad
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-10-24 13:22:55 +0300

    finished product information and reviews styling

--- src/components/store/store.productList.productCard.js
+++ src/components/store/store.productList.productCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Col, Card, Button } from "react-bootstrap";
+import { Row, Col, Card, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class StoreProductListProductCard extends Component {
@@ -13,22 +13,25 @@ class StoreProductListProductCard extends Component {
   };
   render() {
     return (
-      <Col>
-        <Card
-          style={{ width: "18rem" }}
-          onClick={() => this.productClickHandler()}
-        >
-          <Card.Img variant="top" src={this.props.image} />
-          <Card.Body>
-            <Card.Text>
-              <p>{this.props.name}</p>
-            </Card.Text>
-            <Card.Text>
-              <p className="h3">{this.props.price} JD</p>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
+      <Row>
+        <Col className="d-flex justify-content-center align-items-center">
+          <Card
+            style={{ width: "18rem" }}
+            className="my-5 "
+            onClick={() => this.productClickHandler()}
+          >
+            <Card.Img variant="top" src={this.props.image} />
+            <Card.Body>
+              <Card.Text>
+                <p>{this.props.name}</p>
+              </Card.Text>
+              <Card.Text>
+                <p className="h3">{this.props.price} JD</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     );
   }
 }
